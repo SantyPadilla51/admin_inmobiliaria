cc39412dae2391750205641e8f56790e61f78019
Santiago Padilla <padillasantiago51@gmail.com>
2026-08-06 00:53:30 -0300

    update url axios

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://backend-inmobiliaria-argenta.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
